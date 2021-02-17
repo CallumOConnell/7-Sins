52b1b2aa47c15064a91dbe407675923ad395e078
Callum O'Connell <creepcoding@gmail.com>
2021-02-17 12:10:50 +0000

    Camera and Player Controller
    Implemented the basis of the character and camera controller

---
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Sins.Player
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _player;
+
+        [SerializeField]
+        private Camera _camera;
+
+        [SerializeField]
+        private LayerMask _groundLayer;
+
+        private NavMeshAgent _agent;
+
+        private void Awake()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out RaycastHit hit, 100, _groundLayer))
+                {
+                    _agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
