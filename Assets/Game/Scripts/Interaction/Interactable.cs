225b6f050cb76461aa16d03618e429a98cb2d2b7
Callum O'Connell <creepcoding@gmail.com>
2021-04-08 20:47:56 +0100

    Refined scene to just be for character controller

--- Assets/Game/Scripts/Interaction/Interactable.cs
+++ Assets/Game/Scripts/Interaction/Interactable.cs
@@ -1,55 +1,56 @@
 ﻿using UnityEngine;
 
-public class Interactable : MonoBehaviour
+namespace Sins.Interaction
 {
-    [SerializeField]
-    private float _radius = 3f;
-
-    bool isFocus = false, hasInteracted = false;
-
-    Transform player;
-
-
-
-    public float Radius { get => _radius; set => _radius = value; }
-
-    private void OnDrawGizmosSelected()
+    public class Interactable : MonoBehaviour
     {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, _radius);
-    }
+        [SerializeField]
+        private float _radius = 3f;
 
-    private void Update()
-    {
-        if (isFocus && !hasInteracted)
+        private bool isFocus = false, hasInteracted = false;
+
+        private Transform player;
+
+        public float Radius { get => _radius; set => _radius = value; }
+
+        private void OnDrawGizmosSelected()
         {
-            float distance = Vector3.Distance(player.position, transform.position);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, _radius);
+        }
 
-            if (distance <= _radius)
+        private void Update()
+        {
+            if (isFocus && !hasInteracted)
             {
-                Debug.Log("Interact");
+                float distance = Vector3.Distance(player.position, transform.position);
 
-                hasInteracted = true;
+                if (distance <= _radius)
+                {
+                    Debug.Log("Interact");
+
+                    hasInteracted = true;
+                }
             }
         }
-    }
 
-    public virtual void Interact()
-    {
-        Debug.Log($"Interacted with {transform.name}");
-    }
+        public virtual void Interact()
+        {
+            Debug.Log($"Interacted with {transform.name}");
+        }
 
-    public void OnFocused(Transform playerTransform)
-    {
-        isFocus = true;
-        player = playerTransform;
-        hasInteracted = false;
-    }
+        public void OnFocused(Transform playerTransform)
+        {
+            isFocus = true;
+            player = playerTransform;
+            hasInteracted = false;
+        }
 
-    public void OnDefocused()
-    {
-        isFocus = false;
-        player = null;
-        hasInteracted = false;
+        public void OnDefocused()
+        {
+            isFocus = false;
+            player = null;
+            hasInteracted = false;
+        }
     }
 }
--- Assets/Game/Scripts/Player/PlayerController.cs
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -21,18 +21,27 @@ namespace Sins.Player
 
         private Interactable _focus;
 
-        private void Awake()
-        {
-            _agent = GetComponent<NavMeshAgent>();
-        }
+        private void Awake() => _agent = GetComponent<NavMeshAgent>();
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0)) // Left mouse button
             {
                 var ray = _camera.ScreenPointToRay(Input.mousePosition);
 
                 if (Physics.Raycast(ray, out RaycastHit hit, 100, _groundLayer))
+                {
+                    _agent.SetDestination(hit.point);
+
+                    RemoveFocus();
+                }
+            }
+
+            if (Input.GetMouseButtonDown(1)) // Right mouse button
+            {
+                var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out RaycastHit hit, 100))
                 {
                     Interactable interactable = hit.collider.GetComponent<Interactable>();
 
@@ -41,16 +50,15 @@ namespace Sins.Player
                         SetFocus(interactable);
                     }
 
-
-
-
-                    //_agent.SetDestination(hit.point);
+                    _agent.SetDestination(hit.point);
                 }
             }
 
             if (_target != null)
             {
                 _agent.SetDestination(_target.position);
+
+                FaceTarget();
             }
         }
 
@@ -93,7 +101,9 @@ namespace Sins.Player
 
         private void StopFollowingTarget()
         {
+            _agent.stoppingDistance = 0f;
             _agent.updateRotation = true;
+
             _target = null;
         }
 
@@ -104,44 +114,6 @@ namespace Sins.Player
             var lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
 
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
-        }
-
-        private void Test()
-        {
-            float speed = 10;
-
-            if (speed < 20)
-            {
-                // Stay alert
-            }
-            else if (speed <= 20 && speed < 30)
-            {
-                bool peopleCrossing = true;
-
-                if (peopleCrossing)
-                {
-                    // Stop
-                }
-                else
-                {
-                    // GO
-                }
-            }
-            else if (speed <= 30 && speed <= 70)
-            {
-                string weather = "R";
-
-                if (weather == "R")
-                {
-                    // 4 second gap
-                }
-                else
-                {
-                    // 2 second gap
-                }
-            }
-
-
         }
     }
 }
